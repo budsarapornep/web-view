8b3a9c06a1ce5c15d08d006a771d88f356785337
Budsaraporn <budsaraporn.a.ep@gmail.com>
2020-10-09 16:02:14 +0700

    init web view

---
+++ lib/home_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'web_view_container.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _value = 1;
+  final _urlText = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Web view"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(left: 20.0, bottom: 10.0, top: 20.0),
+              child: DropdownButton(
+                  value: _value,
+                  items: [
+                    DropdownMenuItem(
+                      child: Text("https://"),
+                      value: 1,
+                    ),
+                    DropdownMenuItem(
+                      child: Text("http://"),
+                      value: 2,
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _value = value;
+                    });
+                  }),
+            ),
+            Container(
+                padding: EdgeInsets.only(left: 20.0, bottom: 10.0, right: 20.0),
+                child: TextField(
+                  controller: _urlText,
+                )),
+            Container(
+                padding: EdgeInsets.all(20.0),
+                child: FlatButton(
+                  color: Theme.of(context).primaryColor,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 50.0, vertical: 15.0),
+                  child: Text(
+                    'Sumit',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    var http;
+                    if (_value == 1) {
+                      http = "https://";
+                      print(http + _urlText.text);
+                      _handleURLButtonPress(context, http + _urlText.text);
+                    } else {
+                      http = "http://";
+                      print(http + _urlText.text);
+                      _handleURLButtonPress(context, http + _urlText.text);
+                    }
+                  },
+                ))
+          ],
+        ));
+  }
+}
+// class Home extends StatelessWidget {
+//   final _links = ['https://camellabs.com'];
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//     return Scaffold(
+//         body: SafeArea(
+//             child: SingleChildScrollView(
+//                 child: Column(
+//       mainAxisAlignment: MainAxisAlignment.center,
+//       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       children: <Widget>[
+
+//       ],
+//       // children: _links.map((link) => _urlButton(context, link)).toList(),
+//     ))));
+//   }
+
+//   Widget _urlButton(BuildContext context, String url) {
+//     return Container(
+//         padding: EdgeInsets.all(20.0),
+//         child: FlatButton(
+//           color: Theme.of(context).primaryColor,
+//           padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+//           child: Text(url),
+//           onPressed: () => _handleURLButtonPress(context, url),
+//         ));
+//   }
+
+void _handleURLButtonPress(BuildContext context, String url) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => WebViewContainer(url)));
+}
+// }
---
+++ lib/web_view_container.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewContainer extends StatefulWidget {
+  final url;
+
+  WebViewContainer(this.url);
+
+  @override
+  createState() => _WebViewContainerState(this.url);
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  var _url;
+  _WebViewContainerState(this._url);
+  final _key = UniqueKey();
+  WebViewController controller;
+
+  final Completer<WebViewController> _controllerCompleter =
+      Completer<WebViewController>();
+
+  Future<void> _onWillPop(BuildContext context) async {
+    print("onwillpop");
+    if (await controller.canGoBack()) {
+      controller.goBack();
+    } else {
+      // exit(0); //io
+      print('back');
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: Text('กลับไปหน้าเมนู URL'),
+                actions: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('ไม่'),
+                  ),
+                  FlatButton(
+                    onPressed: () {
+                      // SystemNavigator.pop(); //service
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('ใช่'),
+                  ),
+                ],
+              ));
+      return Future.value(false);
+    }
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       appBar: AppBar(),
+  //       body: Column(
+  //         children: [
+  //           Expanded(
+  //               child: WebView(
+  //                   key: _key,
+  //                   javascriptMode: JavascriptMode.unrestricted,
+  //                   initialUrl: _url))
+  //         ],
+  //       ));
+  // }
+
+  //webview handle
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          // title: Text("Sample"),
+          centerTitle: true,
+        ),
+        body: WebView(
+          initialUrl: _url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController c) {
+            _controllerCompleter.future.then((value) => controller = value);
+            _controllerCompleter.complete(c);
+          },
+        ),
+      ),
+    );
+  }
+}
